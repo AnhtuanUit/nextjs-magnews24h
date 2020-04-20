3bd065517b75ed1c47859041d1f8d1c7bf3266dc
AnhtuanUit <anhtuan7194@gmail.com>
2020-04-20 21:08:01 +0700

    Remove unuse javascript

--- pages/_app.js
+++ pages/_app.js
@@ -21,8 +21,6 @@ function App({ Component, pageProps }) {
     <Component {...pageProps} />
     <FooterComponent />
     <script type="text/javascript" src="js/jquery.min.js"></script>
-    <script type="text/javascript" src="js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="js/owl.carousel.min.js"></script>
     <script type="text/javascript" src="js/main.js"></script>
   </div>
 }
