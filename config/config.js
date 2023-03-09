8010a7191056da38e13795e3ef04bd9275bd8559
AnhtuanUit <anhtuan7194@gmail.com>
2023-03-09 15:52:02 +0700

    Update mock API url

--- config/config.js
+++ config/config.js
@@ -1,3 +1,4 @@
 export default {
-    baseApiUrl: 'https://at10346.chickenkiller.com'
+    // baseApiUrl: 'https://at10346.chickenkiller.com'
+    baseApiUrl: 'https://9a47160c-43d0-4889-9e21-41024001c57d.mock.pstmn.io'
 }
