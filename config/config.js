3656626b7c3a80ab3450ec347e6e4630de74acc1
AnhtuanUit <anhtuan7194@gmail.com>
2023-04-29 20:47:13 +0700

    Update api

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
 export default {
     // baseApiUrl: 'https://at10346.chickenkiller.com'
-    baseApiUrl: 'https://9a47160c-43d0-4889-9e21-41024001c57d.mock.pstmn.io'
+    // baseApiUrl: 'https://9a47160c-43d0-4889-9e21-41024001c57d.mock.pstmn.io'
+    baseApiUrl: 'https://magnews24h-api.netlify.app/.netlify/functions'
 }
